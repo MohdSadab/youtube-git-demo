3b54c9953d87e051700d7f66c76d60a17ff447fd
MohdSadab <sadabkhan14198@gmail.com>
2022-05-15 12:38:26 +0530

    basic git command

---
+++ filev2.js
@@ -0,0 +1,6 @@
+// my first functionality is added
+// my second functionality added
+// Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nemo corrupti omnis est?
+// kfp[fp[[pef]]] Eos accusantium debitis quaerat neque enim cupiditate, id quisquf;lkkl;fkl;f;lkfeam nam a
+// periam hic odio unde consequuntur molesfkjlfjkljklflktias aliquid tenetur.
+// new functionality added
---
+++ filev3.js
@@ -0,0 +1,14 @@
+// my first functionality is added
+// my second functionality added
+// Lorem, ipsum dolor sit amet consectetur
+// my new functionality some addition adipisicing elit. Nemo corrupti omnis est?
+// kfp[fp[[pef]]] Eos accusantium debitis quaerat neque enim cupiditate, id quisquf;lkkl;fkl;f;lkfeam nam a
+// periam hic odio unde consequuntur molesfkjlfjkljklflktias aliquid tenetur.
+// new functionality added
+// vs code editor free to use 
+//  cd (change directory)
+// shhshshdh
+
+//  git status  (current status of git repository)
+//  git add (file_path) or (file_name) or . (current directory)   // add the changes to staging area
+//  git rm --cached (file_path) or (file_name)  // move to the modified stage
